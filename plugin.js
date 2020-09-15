8be8d95f369389e2fb9c8cd04b92cfd3a6431ec7
omidpk <omid_khiabani@yahoo.com>
2020-09-15 12:52:04 +0300

    1.0.0.1

--- plugin.js
+++ plugin.js
@@ -1,16 +1,41 @@
 (function($) {
 	$.fn.oSelect = function() {
 		let random = Math.floor(Math.random() * 999999999);
-		this.addClass("oselect").attr("data-select","os-"+random);
 		
-		
+		this.addClass("oSelect").attr("data-select","os-"+random);
+		let container = '<div class="oSelect-container" data-select="os-'+random+'"></div>';
 		this.after(container);
+		
+		
 		this.init = function(){
-			let options = this.get();
+			return this.setStyle();
 		};
-		this.get = function(){
+		this.getOptions = function(){
 			let options = this[0].options;
-			return options;
+			let array = new Array();
+			$.each(options,function(i,v){
+				let color = $(v).attr("data-color");
+				let bg = $(v).attr("data-bg");
+				let value= '';
+				if(!color)
+					color = "#000";
+				if(!bg)
+					bg = "#fff";
+				array.push({bg:bg,color:color,label:v.label,value:v.attributes.value.nodeValue,index:i});
+			});
+			return array;
+		};
+		this.setStyle = function(){
+			let options = this.getOptions();
+			let cnt = $(".oSelect-container[data-select='os-"+random+"']")[0];
+			let cntInner = '<ul class="oSelect-list" data-select="os-'+random+'">';
+			$.each(options,function(i,v){
+				console.log(v);
+				cntInner += '<li style="background-color:'+v.bg+';color:'+v.color+'">'+v.label+'</li>';
+			});
+			
+			cntInner += '</ul>';
+			cnt.innerHTML = cntInner;
 		};
 		return this.init();
 	};
